44944561e581149caa79fc7bf6579d1ad312d79a
tobyl <tobyleftly@gmail.com>
2020-06-17 13:00:46 -0400

    added back ups and downs.

--- src/SingleItem/index.js
+++ src/SingleItem/index.js
@@ -1,16 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import classNames from 'classnames'
 import { HelpRow, ValueRow, WaiverRow, VehicleRow } from './Rows'
 
 import './style.scss'
 
 const SingleItem = ({ name, currentPackage, coverages, isCustomized }) => {
 
+  const ref = useRef()
+
   const [helpVisible, setHelpVisible] = useState(false)
+  const [dir, setDir] = useState(null)
 
   const value = coverages[currentPackage][name]
 
+  useEffect(() => {
+    if (ref.current !== value) {
+      if (value === 'No coverage') {
+        setDir('down')
+      } else if (Number(value) < Number(ref.current)) {
+        setDir('down')
+      } else {
+        setDir('up')
+      }
+    }
+    ref.current = value
+    setTimeout(() => {
+      setDir(null)
+    }, 2000)
+  }, [ref, value, name])
+
+  const cls = classNames('SingleItem', {
+    'Increased': dir === 'up',
+    'Decreased': dir === 'down',
+  })
+
   return (
-    <div className="SingleItem">
+    <div className={cls}>
       {typeof value === 'boolean' ?
         <WaiverRow
           name={name}
